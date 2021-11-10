abb19c2c5918686b59e7affc84ba79e4920ff6ad
yudhiesh <yudhiesh1997@gmail.com>
2021-11-10 15:28:07 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='    Testing software is vital to ensure that code behaves as expected. In Machine Learning projects, testing is not as widely common as normal software testing. The aim of this talk is to give a brief overview on unit testing and to show how a Data Scientist/Machine Learning Engineer can implement it in a modern Machine Learning Development Lifecycle along with DevOps principles such as CI/CD.',
+    author='Yudhiesh Ravindranath | MoneyLion',
+    license='MIT',
+)
