c78d1fb0a26403f6bf5af721e04599cb8bb46367
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-11-30 17:01:45 +0800

    updated description for inference_time

--- conftest.py
+++ conftest.py
@@ -8,7 +8,8 @@ import pytest
 def pytest_addoption(parser):
     parser.addini("rmse", "Min RMSE score for a model to past post-train test")
     parser.addini(
-        "inference_time", "Max inference time for the model to be making predictions"
+        "inference_time",
+        "Max inference time for the model to be making predictions at the 99th percentile",
     )
 
 
