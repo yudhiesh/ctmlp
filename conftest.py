da7a3da30cdca6e6227cc2777f793b7ae37af83c
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-11-30 16:59:46 +0800

    added invariance test and inference test

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 import pickle
 
 import pytest
@@ -14,54 +15,51 @@ def pytest_addoption(parser):
 @pytest.fixture
 def dummy_house():
     return {
-        "Id": {33: 34},
-        "MSSubClass": {33: 20},  # Identifies the type of dwelling involved in the sale.
-        "LotFrontage": {33: 70.0},  # Linear feet of street connected to property
-        "LotArea": {33: 10552},  # Lot size in square feet
-        "OverallQual": {33: 5},  # Rates the overall material and finish of the house
-        "OverallCond": {33: 5},  # Rates the overall condition of the house
-        "YearBuilt": {33: 1959},  # Original construction date
-        "YearRemodAdd": {
-            33: 1959
-        },  # Remodel date (same as construction date if no remodeling or additions)
-        "MasVnrArea": {33: 0.0},  # Masonry veneer area in square feet
-        "BsmtFinSF1": {33: 1018},  # Type 1 finished square feet
-        "BsmtFinSF2": {33: 0},  # Type 2 finished square feet
-        "BsmtUnfSF": {33: 380},  #  Unfinished square feet of basement area
-        "TotalBsmtSF": {33: 1398},  # Total square feet of basement area
-        "1stFlrSF": {33: 1700},  # First Floor square feet
-        "2ndFlrSF": {33: 0},  # Second floor square feet
-        "LowQualFinSF": {33: 0},  # Low quality finished square feet (all floors)
-        "GrLivArea": {33: 1700},  # Above grade (ground) living area square feet
-        "BsmtFullBath": {33: 0},  # Basement full bathrooms
-        "BsmtHalfBath": {33: 1},  # Basement half bathrooms
-        "FullBath": {33: 1},  # Full bathrooms above grade
-        "HalfBath": {33: 1},  #  Half baths above grade
-        "BedroomAbvGr": {33: 4},
-        "KitchenAbvGr": {33: 1},
-        "TotRmsAbvGrd": {33: 6},  # Total rooms above grade (does not include bathrooms)
-        "Fireplaces": {33: 1},  # Number of fireplaces
-        "GarageYrBlt": {33: 1959.0},  # Year garage was built
-        "GarageCars": {33: 2},  # Size of garage in car capacity
-        "GarageArea": {33: 447},  # Size of garage in square feet
-        "WoodDeckSF": {33: 0},  # Wood deck area in square feet
-        "OpenPorchSF": {33: 38},  # Open porch area in square feet
-        "EnclosedPorch": {33: 0},  # Enclosed porch area in square feet
-        "3SsnPorch": {33: 0},  # Three season porch area in square feet
-        "ScreenPorch": {33: 0},  # Screen porch area in square feet
-        "PoolArea": {33: 0},  # Pool area in square feet
-        "MiscVal": {33: 0},  # $Value of miscellaneous feature
-        "MoSold": {33: 4},  # Month Sold (MM)
-        "YrSold": {33: 2010},  # Year Sold (YYYY)
-        "SalePrice": {33: 165500},
+        "Id": 34,
+        "MSSubClass": 20,  # Identifies the type of dwelling involved in the sale.
+        "LotFrontage": 70.0,  # Linear feet of street connected to property
+        "LotArea": 10552,  # Lot size in square feet
+        "OverallQual": 5,  # Rates the overall material and finish of the house
+        "OverallCond": 5,  # Rates the overall condition of the house
+        "YearBuilt": 1959,  # Original construction date
+        "YearRemodAdd": 1959,  # Remodel date (same as construction date if no remodeling or additions)
+        "MasVnrArea": 0.0,  # Masonry veneer area in square feet
+        "BsmtFinSF1": 1018,  # Type 1 finished square feet
+        "BsmtFinSF2": 0,  # Type 2 finished square feet
+        "BsmtUnfSF": 380,  #  Unfinished square feet of basement area
+        "TotalBsmtSF": 1398,  # Total square feet of basement area
+        "1stFlrSF": 1700,  # First Floor square feet
+        "2ndFlrSF": 0,  # Second floor square feet
+        "LowQualFinSF": 0,  # Low quality finished square feet (all floors)
+        "GrLivArea": 1700,  # Above grade (ground) living area square feet
+        "BsmtFullBath": 0,  # Basement full bathrooms
+        "BsmtHalfBath": 1,  # Basement half bathrooms
+        "FullBath": 1,  # Full bathrooms above grade
+        "HalfBath": 1,  #  Half baths above grade
+        "BedroomAbvGr": 4,
+        "KitchenAbvGr": 1,
+        "TotRmsAbvGrd": 6,  # Total rooms above grade (does not include bathrooms)
+        "Fireplaces": 1,  # Number of fireplaces
+        "GarageYrBlt": 1959.0,  # Year garage was built
+        "GarageCars": 2,  # Size of garage in car capacity
+        "GarageArea": 447,  # Size of garage in square feet
+        "WoodDeckSF": 0,  # Wood deck area in square feet
+        "OpenPorchSF": 38,  # Open porch area in square feet
+        "EnclosedPorch": 0,  # Enclosed porch area in square feet
+        "3SsnPorch": 0,  # Three season porch area in square feet
+        "ScreenPorch": 0,  # Screen porch area in square feet
+        "PoolArea": 0,  # Pool area in square feet
+        "MiscVal": 0,  # $Value of miscellaneous feature
+        "MoSold": 4,  # Month Sold (MM)
+        "YrSold": 2010,  # Year Sold (YYYY)
+        "SalePrice": 165500,
     }
 
 
 @pytest.fixture
-def load_model():
+def model():
     filename = "./models/model.pkl"
-    model = pickle.load(open(filename, "rb"))
-    return model
+    return pickle.load(open(filename, "rb"))
 
 
 @pytest.fixture
@@ -71,3 +69,8 @@ def model_metrics():
         metrics = json.load(f)
     return metrics
 
+
+@pytest.fixture
+def dataset():
+    filename = "./data/raw/test.csv"
+    return pd.read_csv(filename)
--- tests/test_post_train.py
+++ tests/test_post_train.py
@@ -1,18 +1,73 @@
+import numpy as np
+import pandas as pd
+import copy
+import math
+import time
+
+
+def get_features_labels(df: pd.DataFrame, target_column: str):
+    """
+    Splits the dataframe into features and labels
+    """
+    feature_columns = [
+        column for column in df.columns.tolist() if column != target_column
+    ]
+    X = df.loc[:, feature_columns]
+    y = df.loc[:, target_column]
+    X = X.fillna(X.mean())
+    return X, y
+
+
 def test_config(request):
     score = request.config.getini("rmse")
     assert int(score) == 40_000
 
 
-def test_invariance_tests(dummy_house):
-    ...
+# Keeping all except 1 feature at a time the same
+# Changing these features a bit should not result in a noticeable difference
+# in the models prediction with the ground truth
+def test_invariance_tests(model, dummy_house):
+    # Changing the year built a bit shouldn't really increase a houses value
+    dummy_house_copy = copy.deepcopy(dummy_house)
+    year_built = dummy_house.get("YearBuilt")
+    dummy_house_copy["YearBuilt"] = year_built + 1
+    changed_df = pd.DataFrame.from_dict([dummy_house], orient="columns")
+    unchanged_df = pd.DataFrame.from_dict([dummy_house_copy], orient="columns")
+    changed_X, _ = get_features_labels(changed_df, target_column="SalePrice")
+    unchanged_X, _ = get_features_labels(unchanged_df, target_column="SalePrice")
+    changed_score = model.predict(changed_X)
+    unchanged_score = model.predict(unchanged_X)
+    # check that there's about max $5k difference between unchanged and changed
+    # house prices
+    # $5k is something I feel makes sense, obviously domain knowledge plays a big
+    # role in coming up with these test parameters
+    assert math.isclose(changed_score, unchanged_score, rel_tol=5e3)
 
 
+# Keeping all except 1 feature at a time the same
+# Chaning these features a bit should result in a notieceable difference
 def test_directional_expectation_tests():
     ...
 
 
-def test_model_inference_times():
-    ...
+def time_predict(model, df):
+    """
+    Helper function to time the run of a single batch prediction of a model
+    """
+    start = time.time()
+    model.predict(df)
+    end = time.time()
+    return end - start
+
+
+def test_model_inference_times(request, dataset, model):
+    X, _ = get_features_labels(dataset, target_column="SalePrice")
+    latency = np.array([time_predict(model, X) for _ in range(100)])
+    latency_p99 = np.quantile(latency, 0.99)
+    inference_time = float(request.config.getini("inference_time"))
+    assert (
+        latency_p99 < inference_time
+    ), f"Prediction time at the 99th percentile should be < {inference_time} but was {latency_p99}"
 
 
 def test_model_metric(request, model_metrics):
