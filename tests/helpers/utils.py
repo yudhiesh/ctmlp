5e9eba8da8809dd00aba989f8819e327791e090c
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-11-30 21:22:59 +0800

    moved test helper methods to helpers

---
+++ tests/helpers/utils.py
@@ -0,0 +1,74 @@
+import copy
+import time
+from typing import Any, Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+
+SCORES = Tuple[np.ndarray, np.ndarray]
+
+
+def get_features_labels(
+    df: pd.DataFrame, target_column: str,
+) -> Tuple[
+    pd.DataFrame, pd.DataFrame,
+]:
+    """
+    Splits the dataframe into features and labels
+    """
+    feature_columns = [
+        column for column in df.columns.tolist() if column != target_column
+    ]
+    X = df.loc[:, feature_columns]
+    y = df.loc[:, target_column]
+    X = X.fillna(X.mean())
+    return X, y
+
+
+def time_predict(model: Any, df: pd.DataFrame) -> float:
+    """
+    Helper function to time the run of a single batch prediction of a model
+    """
+    start = time.time()
+    model.predict(df)
+    end = time.time()
+    return end - start
+
+
+def get_predictions(model: Any, data: pd.DataFrame, data_copy: pd.DataFrame,) -> SCORES:
+    """
+    Helper function to compare predictions of two different dataframes using
+    a model
+    """
+    changed_df = pd.DataFrame.from_dict([data_copy], orient="columns")
+    unchanged_df = pd.DataFrame.from_dict([data], orient="columns")
+
+    changed_X, _ = get_features_labels(changed_df, target_column="SalePrice")
+    unchanged_X, _ = get_features_labels(unchanged_df, target_column="SalePrice")
+
+    changed_score = model.predict(changed_X)
+    unchanged_score = model.predict(unchanged_X)
+    return changed_score, unchanged_score
+
+
+def get_test_case(
+    data: pd.DataFrame,
+    model: Any,
+    key: str,
+    add: Optional[Union[int, float]] = None,
+    multiply: Optional[Union[int, float]] = None,
+) -> SCORES:
+    """
+    Helper method to create the different test cases that alter the keys in a
+    dictionary
+    """
+    data_copy = copy.deepcopy(data)
+    value = data.get(key)
+    if add:
+        data_copy[key] = value + add
+    elif multiply:
+        data_copy[key] = value * multiply
+    else:
+        raise Exception("Please pass either an addition or multiply value")
+    changed_score, unchanged_score = get_predictions(model, data, data_copy)
+    return changed_score, unchanged_score
--- tests/test_post_train.py
+++ tests/test_post_train.py
@@ -1,32 +1,9 @@
-import numpy as np
-import pandas as pd
-import copy
 import math
-import time
+
+import numpy as np
 import pytest
 
-
-def get_features_labels(df: pd.DataFrame, target_column: str):
-    """
-    Splits the dataframe into features and labels
-    """
-    feature_columns = [
-        column for column in df.columns.tolist() if column != target_column
-    ]
-    X = df.loc[:, feature_columns]
-    y = df.loc[:, target_column]
-    X = X.fillna(X.mean())
-    return X, y
-
-
-def time_predict(model, df):
-    """
-    Helper function to time the run of a single batch prediction of a model
-    """
-    start = time.time()
-    model.predict(df)
-    end = time.time()
-    return end - start
+from helpers.utils import get_features_labels, get_test_case, time_predict
 
 
 def test_config(request):
@@ -35,48 +12,15 @@ def test_config(request):
     assert int(score) == 40_000
 
 
-def get_predictions(
-    model, data, data_copy,
-):
-    """
-    Helper function to compare predictions of two different dataframes using
-    a model
-    """
-    changed_df = pd.DataFrame.from_dict([data_copy], orient="columns")
-    unchanged_df = pd.DataFrame.from_dict([data], orient="columns")
-
-    changed_X, _ = get_features_labels(changed_df, target_column="SalePrice")
-    unchanged_X, _ = get_features_labels(unchanged_df, target_column="SalePrice")
-
-    changed_score = model.predict(changed_X)
-    unchanged_score = model.predict(unchanged_X)
-    return changed_score, unchanged_score
-
-
-def get_test_case(data, model, key, add=None, multiply=None):
-    """
-    Helper method to create the different test cases that alter the keys in a
-    dictionary
-    """
-    data_copy = copy.deepcopy(data)
-    value = data.get(key)
-    if add:
-        data_copy[key] = value + add
-    elif multiply:
-        data_copy[key] = value * multiply
-    else:
-        raise Exception("Please pass either an addition or multiply value")
-    changed_score, unchanged_score = get_predictions(model, data, data_copy)
-    return changed_score, unchanged_score
-
-
-# Keeping all except 1 feature at a time the same
-# Changing these features a bit should not result in a noticeable difference
-# in the models prediction with the ground truth
 @pytest.mark.parametrize(
     "kwargs", [({"key": "YearBuilt", "add": 1}), ({"key": "LotFrontage", "add": 5}),]
 )
 def test_invariance_tests(kwargs, model, dummy_house):
+    """
+    Keeping all except 1 feature at a time the same
+    Changing these features a bit should not result in a noticeable difference
+    in the models prediction with the ground truth
+    """
     changed_score, unchanged_score = get_test_case(dummy_house, model, **kwargs)
     # check that there's about max $5k difference between unchanged and changed
     # house prices
@@ -85,8 +29,6 @@ def test_invariance_tests(kwargs, model, dummy_house):
     assert math.isclose(changed_score, unchanged_score, rel_tol=5e3)
 
 
-# Keeping all except 1 feature at a time the same
-# Chaning these features a bit should result in a notieceable difference
 # TODO: Add in argument to parametrize to handle cases where a feature should
 # negatively impact price
 @pytest.mark.parametrize(
@@ -103,6 +45,10 @@ def test_invariance_tests(kwargs, model, dummy_house):
 def test_directional_expectation_tests(
     kwargs, model, dummy_house,
 ):
+    """
+    Keeping all except 1 feature at a time the same
+    Chaning these features a bit should result in a notieceable difference
+    """
     changed_score, unchanged_score = get_test_case(dummy_house, model, **kwargs)
     assert changed_score > unchanged_score
 
