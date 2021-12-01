d308b32afb90078806a362ed5605ab9b58bf9707
jteh-ml <78783888+jteh-ml@users.noreply.github.com>
2021-12-01 13:08:33 +0800

    edit line 133

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -130,9 +130,7 @@ class TrainModel:
         model = self.get_model()
         model.fit(batch_X, batch_y)
         y_pred = model.predict(batch_X)
-        if not np.allclose(y_pred, batch_y):
-            return False
-        return True
+        return np.allclose(y_pred, batch_y):
 
     def get_model(self) -> Any:
         """Returns the current model class that is being used"""
